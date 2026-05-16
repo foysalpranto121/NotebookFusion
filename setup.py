27fc0b2e6b70f1feec24533a2616b98620a3afeb
foysalpranto121 <foysalpranto2002@gmail.com>
2026-05-16 17:44:15 +0600

    Fix setup.py and add website rendering module

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
 import setuptools
 
-with open("README.md", "r",encoding="utf-8") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-
-
-    __version__ = "0.0.2"
-    REPO_NAME = "NotebookFusion"
-    AUTHOR_USER_NAME = "foysalpranto121"
-    AUTHOR_EMAIL = "foysalpranto2002@gmail.com"
-    SRC_REPO = "NotebookFusion"
+__version__ = "0.0.2"
+REPO_NAME = "NotebookFusion"
+AUTHOR_USER_NAME = "foysalpranto121"
+AUTHOR_EMAIL = "foysalpranto2002@gmail.com"
+SRC_REPO = "NotebookFusion"
 
 setuptools.setup(
-    name=SRC_REPO,  
-    version=_version,
+    name=SRC_REPO,
+    version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A Python package for  notebooks.",
+    description="A Python package for enhanced Jupyter notebook functionality.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
@@ -24,4 +22,10 @@ setuptools.setup(
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
     },
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"))
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        "ensure>=1.0.2",
+        "py-youtube>=1.1.7",
+    ],
+    python_requires=">=3.8",
+)
